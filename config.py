3ff7188640c2ef3acb26f99fb9175fc2c60fe431
Root-X-Admin <ayushhande052@gmail.com>
2025-06-14 21:45:37 +0530

    Refactor configuration in config.py; update SQLALCHEMY_DATABASE_URI and adjust Flask-Mail settings for improved environment variable usage.

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ load_dotenv()  # Make sure to load .env variables
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///habit_tracker.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///habit_tracker.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = 'filesystem'
 
-    # Flask-Mail Configuration for SendGrid
-    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.sendgrid.net')
+    # Flask-Mail Configuration (Brevo)
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
     MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'True') == 'True'
     MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'False') == 'True'
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'yzD_vVb7SYWYg6MUOOabAw')  
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
