9dd08b38370170faa6f84bb0e60ffee426ebee2e
Root-X-Admin <ayushhande052@gmail.com>
2025-06-15 17:56:09 +0530

    Fix Flask-Mail configuration: update MAIL_USE_TLS default value to 'False' for improved security.

--- config.py
+++ config.py
@@ -13,8 +13,9 @@ class Config:
     # Flask-Mail Configuration (Brevo)
     MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'True') == 'True'
-    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'False') == 'True'
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'False') == 'True'
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'True') == 'True'
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
+    
